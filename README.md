fbe33dee817e6638053960a267c3f4b8b8fb7023
‘iamlfc’ <iamlifuchang@163.com>
2023-01-11 10:07:36 +0800

    增加获取ip地址方法

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 安卓本地服务器启用
 
 学习来源
-https://blog.csdn.net/sunbinkang/article/details/124305235
+https://blog.csdn.net/sunbinkang/article/details/124305235                                                                                                                                                                                                                                                           
--- app/src/main/java/com/lfc/mynanohttpddemo/MainActivity.kt
+++ app/src/main/java/com/lfc/mynanohttpddemo/MainActivity.kt
@@ -1,17 +1,34 @@
 package com.lfc.mynanohttpddemo
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.net.wifi.WifiManager
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import java.io.IOException
+import java.net.Inet4Address
+import java.net.NetworkInterface
+import java.net.SocketException
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private val mBtnStartService: Button by lazy { findViewById<Button>(R.id.btn_start_service) }
+    private val mTvIpinfo: TextView by lazy { findViewById<TextView>(R.id.tv_ipinfo) }
 
-        private var httpService = MyHttpService(HTTP_IP, HTTP_PORT)
-//    private var httpService = MyHttpServiceWeb("127.0.0.1", 9988)
+    private var httpService = MyHttpService(HTTP_IP, HTTP_PORT)
+
+    //    private var httpService = MyHttpServiceWeb("127.0.0.1", 9988)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initView()
+    }
+
+    private fun initView() {
+
+        mTvIpinfo.setText(getLocalIPAddress(this))
     }
 
     override fun onClick(p0: View?) {
@@ -36,7 +53,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             println("服务启动完成")
         } catch (e: IOException) {
             e.printStackTrace()
-            println("服务启动错误  "+e.printStackTrace())
+            println("服务启动错误  " + e.printStackTrace())
         }
     }
 
@@ -45,4 +62,41 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         super.onDestroy()
         httpService.stop()
     }
+
+    @SuppressLint("DefaultLocale")
+    fun getLocalIPAddress(context: Context): String? {
+        val wifiManager = context.getSystemService(WIFI_SERVICE) as WifiManager
+        val ipAddress = wifiManager.connectionInfo.ipAddress
+        if (ipAddress == 0) {
+            try {
+                val enumerationNi = NetworkInterface.getNetworkInterfaces()
+                while (enumerationNi.hasMoreElements()) {
+                    val networkInterface = enumerationNi.nextElement()
+                    val interfaceName = networkInterface.displayName
+                    if (interfaceName == "eth0" || interfaceName == "wlan0") {
+                        val enumIpAddr = networkInterface.inetAddresses
+                        while (enumIpAddr.hasMoreElements()) {
+                            val inetAddress = enumIpAddr.nextElement()
+                            if (!inetAddress.isLoopbackAddress && inetAddress is Inet4Address) {
+                                return inetAddress.getHostAddress()
+                            }
+                        }
+                    }
+                }
+            } catch (e: SocketException) {
+                e.printStackTrace()
+            }
+        } else {
+            return String.format(
+                "%d.%d.%d.%d",
+                ipAddress and 0xff,
+                ipAddress shr 8 and 0xff,
+                ipAddress shr 16 and 0xff,
+                ipAddress shr 24 and 0xff
+            )
+        }
+        return "0.0.0.0"
+    }
+
+
 }
